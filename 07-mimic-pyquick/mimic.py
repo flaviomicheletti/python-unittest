12601016d068ea0e5241547a297b4b564c65c34b
Flávio Micheletti <flaviomicheletti@hotmail.com>
2022-11-22 15:44:40 -0300

    no sense

--- 07-mimic-pyquick/mimic.py
+++ 07-mimic-pyquick/mimic.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python -tt
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
 
 """Mimic pyquick exercise -- optional extra exercise.
 Google's Python Class
@@ -46,54 +40,46 @@ import sys
 
 
 def mimic_dict(filename):
-  """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  # LAB(begin solution)
-  mimic_dict = {}
-  f = open(filename, 'r')
-  text = f.read()
-  f.close()
-  words = text.split()
-  prev = ''
-  for word in words:
-    if not prev in mimic_dict:
-      mimic_dict[prev] = [word]
-    else:
-      mimic_dict[prev].append(word)
-    # Could write as: mimic_dict[prev] = mimic_dict.get(prev, []) + [word]
-    # It's one line, but not totally satisfying.
-    prev = word
-  return mimic_dict
-  # LAB(replace solution)
-  # return
-  # LAB(end solution)
+    """Returns mimic dict mapping each word to list of words which follow it."""
+    mimic_dict = {}
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+    words = text.split()
+    prev = ''
+    for word in words:
+        if not prev in mimic_dict:
+            mimic_dict[prev] = [word]
+        else:
+            mimic_dict[prev].append(word)
+        # Could write as: mimic_dict[prev] = mimic_dict.get(prev, []) + [word]
+        # It's one line, but not totally satisfying.
+        prev = word
+    return mimic_dict
+    return
 
 
 def print_mimic(mimic_dict, word):
-  """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  # LAB(begin solution)
-  for unused_i in range(200):
-    print word,
-    nexts = mimic_dict.get(word)          # Returns None if not found
-    if not nexts:
-      nexts = mimic_dict['']  # Fallback to '' if not found
-    word = random.choice(nexts)
-  # The 'unused_' prefix turns off the lint warning about the unused variable.
-  # LAB(replace solution)
-  # return
-  # LAB(end solution)
+    """Given mimic dict and start word, prints 200 random words."""
+    for unused_i in range(200):
+        print(word),
+        # Returns None if not found
+        nexts = mimic_dict.get(word)
+        if not nexts:
+            # Fallback to '' if not found
+            nexts = mimic_dict['']
+        word = random.choice(nexts)
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
-  if len(sys.argv) != 2:
-    print 'usage: ./mimic.py file-to-read'
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print('usage: ./mimic.py file-to-read')
+        sys.exit(1)
 
-  dict = mimic_dict(sys.argv[1])
-  print_mimic(dict, '')
+    dict = mimic_dict(sys.argv[1])
+    print_mimic(dict, '')
 
 
 if __name__ == '__main__':
-  main()
+    main()
---
+++ 07-mimic-pyquick/readme.md
@@ -0,0 +1,5 @@
+# mimic Pyquik
+
+Did you understand this Google's exercise?
+
+I don't, it doesn't make any sense

c311cc0aca4453186c22f61aa2ab3ad712034b0f
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-01-03 22:31:11 -0300

    detail

--- 01-check-working/sys-arg.py
+++ 01-check-working/sys-arg.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python -tt
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
+""" A tiny Python program to check that Python is working.
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
-"""A tiny Python program to check that Python is working.
 Try running this program from the command line like this:
   python hello.py
   python hello.py Alice
 That should print:
   Hello World -or- Hello Alice
 Try changing the 'Hello' to 'Howdy' and run again.
+
 Once you have that working, you're ready for class -- you can edit
 and run Python code; now you just need to learn Python!
 """

b92832297d6bab495ca7f445530585d5430f2ed4
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-01-29 13:03:51 -0300

    calculator

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -2,8 +2,20 @@ from unittest import TestCase
 from unittest.mock import patch
 import calculator
 
+
 class TestCalculator(TestCase):
-    @patch('calculator.Calculator.add', return_value=9)
+    @patch("calculator.Calculator.add", return_value=9)
     def test_add(self, add):
-        expected = add(2, 3)
+        expected = add(5, 5)
         self.assertEqual(expected, 9)
+
+
+def mock_add(x, y):
+    return 11
+
+
+class TestCalculatorSideEffect(TestCase):
+    @patch("calculator.Calculator.add", side_effect=mock_add)
+    def test_add(self, add):
+        expected = add(5, 5)
+        self.assertEqual(expected, 11)

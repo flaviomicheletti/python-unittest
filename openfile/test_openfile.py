e9989176ebc3263498aab4337f0e0a2ff77fa69e
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-02-22 16:17:49 -0300

    openfile

---
+++ openfile/test_openfile.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import mock_open, patch
+
+class TestOpenFile(unittest.TestCase):
+    def test_open_file(self):
+        filename = "test_file.txt"
+        expected_result = "test data"
+        
+        # Create a mock object that returns the expected result
+        mock_file = mock_open(read_data=expected_result)
+        
+        # Patch the built-in `open()` function with the mock object
+        with patch('builtins.open', mock_file):
+            # Call the function that uses `open()`
+            result = open(filename, "r")
+            
+            # Verify that the expected result was returned
+            self.assertEqual(result.read(), expected_result)
+            
+        # Verify that the `open()` function was called with the correct arguments
+        mock_file.assert_called_once_with(filename, "r")
+
+
+def my_open(filename, mode):
+    result = open(filename, "r")
+    return result
+
+class TestMyOpen(unittest.TestCase):
+    def test_my_open(self):
+        filename = "test_file.txt"
+        expected_result = "test data"
+        
+        # Create a mock object that returns the expected result
+        mock_file = mock_open(read_data=expected_result)
+        
+        # Patch the built-in `open()` function with the mock object
+        with patch('builtins.open', mock_file):
+            # Call the `my_open()` function
+            result = my_open(filename, "r")
+            
+            # Verify that the expected result was returned
+            self.assertEqual(result.read(), expected_result)
+            
+        # Verify that the `open()` function was called with the correct arguments
+        mock_file.assert_called_once_with(filename, "r")
+

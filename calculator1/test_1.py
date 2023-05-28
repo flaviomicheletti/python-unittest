5fa8f39dd8347eb5aca159a3e1d4b561fb05e80a
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-05-28 15:23:33 -0300

    calculator1

---
+++ calculator1/test_1.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import patch
+# from calculator1 import add
+
+
+class TestCalculator(TestCase):
+    @patch("calculator1.add", return_value=9)
+    def test_add(self, add):
+        self.assertEqual(add(5, 5), 9)
+
+
+def mock_add(x, y):
+    return 11
+
+
+class TestCalculatorSideEffect(TestCase):
+    @patch("calculator1.add", side_effect=mock_add)
+    def test_add(self, add):
+        self.assertEqual(add(5, 5), 11)
+
+#
+# 50%
+#

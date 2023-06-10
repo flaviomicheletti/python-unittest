9e08e2ec13c94086bfa49ee25d1841a2b2d150fa
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-06-10 13:26:19 -0300

    del files

--- my_module5/__init__.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-class MongoConnection:
-    def __init__(self, db_name, host='localhost', port=27017):
-        self.client = pymongo.MongoClient(host, port)
-        self.db = self.client[db_name]
-
-    def get_collection(self, collection_name):
-        return self.db[collection_name]
